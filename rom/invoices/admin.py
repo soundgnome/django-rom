2acefa9126f68d918516665c3f0783ccc4ebec1e
Terran Olson <terran@halfcadence.net>
2017-02-15 16:54:55 -0800

    adjusting invoice admin

--- rom/invoices/admin.py
+++ rom/invoices/admin.py
@@ -5,6 +5,10 @@ from .models import Client, Expense, Invoice
 class ExpenseAdmin(admin.ModelAdmin):
     fields = ('date', 'amount', 'type')
 
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_number', 'amount', 'client', 'date_received')
+
 admin.site.register(Client)
 admin.site.register(Expense, ExpenseAdmin)
-admin.site.register(Invoice)
+admin.site.register(Invoice, InvoiceAdmin)
